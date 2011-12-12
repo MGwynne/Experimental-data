f315b70cd7f15425af0b1068faaab19ff459639d
Matthew Gwynne <360678@swan.ac.uk>
2011-12-12 00:07:50 +0000

    Adding data for AES(r,1,2,8) for r in 1 to 20.

---
+++ AES/1_2_8/ssaes_r1-20_c2_rw1_e8_f0_k1-20_aes_1base_box_aes_mc_bidirectional/generate.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Matthew Gwynne, 11.12.2011 (Swansea)
+# Copyright 2011 Oliver Kullmann
+# This file is part of the OKlibrary. OKlibrary is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation and included in this library; either version 3 of the 
+# License, or any later version.
+
+# Generating 400 key discovery instances for AES(r,1,2,8) using the 1base
+# translation for r in 1 to 20. For each round random keys with seeds
+# 1 to 20 are used to instantiation the AES instances.
+#
+# We have that 20 rounds * 20 keys = the 400 instances r1_k1.cnf to r20_k20.cnf.
+#
+# All files are in:
+#
+# ssaes_r1-20_c2_rw1_e8_f0_k1-20_aes_1base_box_aes_mc_bidirectional .
+#
+
+set -o errexit
+set -o nounset
+
+num_rows=1
+num_cols=2
+field_size=8
+translation_name=1base
+translation_param=rbase
+mixcolumns=bidirectional # Forward and backward translation
+rounds=20
+keys=20
+
+
+box_generation="output_rijnsbox_fullcnf_stdname();"
+oklib --maxima --batch-string "'print(\"Loading OKlibrary ...\"); oklib_load_all(); num_rows : ${num_rows}; num_columns : ${num_cols}; exp : ${field_size}; final_round_b : false; box_tran : aes_${translation_param}_box; mc_tran : aes_mc_${mixcolumns}; for num_rounds : 1 thru ${rounds} do ( print(\"Generating round \", num_rounds, \" ...\"), output_ss_fcl_std(num_rounds, num_columns, num_rows, exp, final_round_b, box_tran, mc_tran), for seed : 1 thru ${keys} do ( output_ss_random_pc_pair(seed,num_rounds,num_columns,num_rows,exp,final_round_b) )); print(\"Outputting boxes\"); ${box_generation} exit();'" | grep "round\|Loading OKlib\|Outputting" # Using grep to suppress output, but avoid using $ in batch string.
+
+# Generating S-box 1-base
+echo "Generating 1-base ..."
+QuineMcCluskey-n16-O3-DNDEBUG AES_Sbox_full.cnf > AES_Sbox_PI.cnf
+cat AES_Sbox_PI.cnf | RandomShuffleDimacs-O3-DNDEBUG 103 | SortByClauseLength-O3-DNDEBUG > AES_Sbox_PI_sorted.cnf
+RUcpGen-O3-DNDEBUG AES_Sbox_PI_sorted.cnf > AES_Sbox_gen.cnf
+cat AES_Sbox_gen.cnf | RandomShuffleDimacs-O3-DNDEBUG 1 | SortByClauseLengthDescending-O3-DNDEBUG > AES_Sbox_gen_sorted.cnf
+cat AES_Sbox_gen_sorted.cnf | RUcpBase-O3-DNDEBUG > Sbox_8_1base.cnf
+
+
+echo "Generating combined instance+key files ..."
+for r in $(seq 1 ${rounds}); do
+  for s in $(seq 1 ${keys}); do
+    AppendDimacs-O3-DNDEBUG ssaes_r${r}_c${num_cols}_rw${num_rows}_e${field_size}_f0.cnf ssaes_pcpair_r${r}_c${num_cols}_rw${num_rows}_e${field_size}_f0_s${s}.cnf > r${r}_k${s}.cnf;
+  done
+done
+
+
+md5sum --quiet -c md5sum
---
+++ AES/1_2_8/ssaes_r1-20_c2_rw1_e8_f0_k1-20_aes_canon_box_aes_mc_bidirectional/generate.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Matthew Gwynne, 11.12.2011 (Swansea)
+# Copyright 2011 Oliver Kullmann
+# This file is part of the OKlibrary. OKlibrary is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation and included in this library; either version 3 of the 
+# License, or any later version.
+
+# Generating 400 key discovery instances for AES(r,1,2,8) using the canonical
+# translation for r in 1 to 20. For each round random keys with seeds
+# 1 to 20 are used to instantiation the AES instances.
+#
+# We have that 20 rounds * 20 keys = the 400 instances r1_k1.cnf to r20_k20.cnf.
+#
+# All files are in:
+#
+# ssaes_r1-20_c2_rw1_e8_f0_k1-20_aes_canon_box_aes_mc_bidirectional .
+#
+
+set -o errexit
+set -o nounset
+
+num_rows=1
+num_cols=2
+field_size=8
+translation_name=canon
+translation_param=ts # canonical (this will be fixed)
+mixcolumns=bidirectional # Forward and backward translation
+rounds=20
+keys=20
+
+
+oklib --maxima --batch-string "'print(\"Loading OKlibrary ...\"); oklib_load_all(); num_rows : ${num_rows}; num_columns : ${num_cols}; exp : ${field_size}; final_round_b : false; box_tran : aes_${translation_param}_box; mc_tran : aes_mc_${mixcolumns}; for num_rounds : 1 thru ${rounds} do ( print(\"Generating round \", num_rounds, \" ...\"), output_ss_fcl_std(num_rounds, num_columns, num_rows, exp, final_round_b, box_tran, mc_tran), for seed : 1 thru ${keys} do ( output_ss_random_pc_pair(seed,num_rounds,num_columns,num_rows,exp,final_round_b) )); print(\"Outputting boxes\"); ${box_generation} exit();'" | grep "round\|Loading OKlib\|Outputting" # Using grep to suppress output, but avoid using $ in batch string.
+
+
+echo "Generating combined instance+key files ..."
+for r in $(seq 1 ${rounds}); do
+  for s in $(seq 1 ${keys}); do
+    AppendDimacs-O3-DNDEBUG ssaes_r${r}_c${num_cols}_rw${num_rows}_e${field_size}_f0.cnf ssaes_pcpair_r${r}_c${num_cols}_rw${num_rows}_e${field_size}_f0_s${s}.cnf > r${r}_k${s}.cnf;
+  done
+done
+
+
+md5sum --quiet -c md5sum
